b7d4ee0ff77e3878c50867eae8e7616357ce396a
clarevoyance <clarevoyance@me.com>
2020-01-01 16:23:40 +0100

    cs50 2020 update

--- pset2/readability.c
+++ pset2/readability.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
-    string name = get_string("Text:")
-    printf("%s\n", name)
+    string text = get_string("Text:")
+    for (int i = 0, n = strlen(text); i <= nl i++)
+    {
+        
+    }
+
+    printf("%s\n", text)
 }
--- pset6/readability.py
+++ pset6/readability.py
@@ -2,7 +2,15 @@ from cs50 import get_string
 
 def main():
 
+    text = get_string("Text: ")
 
+    lettercount = 0
+    for i in text:
+        if i is not " ":
+            lettercount += 1
+        else:
+            lettercount += 0
+    print(lettercount, "letter(s)")
 
 
 if __name__ == "__main__":
