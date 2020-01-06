c7d4f2f3d7a0d3de690ac38374e6b1f9e5670e03
clarevoyance <clarevoyance@me.com>
2020-01-06 09:50:19 +0100

    pset update

--- pset2/readability.c
+++ pset2/readability.c
@@ -9,20 +9,18 @@ int main(void)
 
     // computing total letter count
     float lettercount = 0;
-    for (int i = 0, n = strlen(text); i < n; i++)
+    for (int i = 0; text[i] != '\0'; i++)
     {
         char l = text[i];
         if ((l >= 97 && l < 123) || (l >= 65 && l < 91))
         {
             lettercount += 1;
-
         }
     }
-    //printf("%f letter(s)\n", lettercount);
 
     // computing total word count
     float wordcount = 1;
-    for (int j = 0, n = strlen(text); j < n; j++)
+    for (int j = 0; text[j] != '\0'; j++)
     {
         char w = text[j];
         if (isspace(w))
@@ -30,11 +28,10 @@ int main(void)
             wordcount += 1;
         }
     }
-    //printf("%f word(s)\n", wordcount);
 
     // computing total sentence count
     float sentencecount = 0;
-    for (int k = 0, n = strlen(text); k < n; k++)
+    for (int k = 0; text[k] != '\0' ; k++)
     {
         char s = text[k];
         if (s == 33 || s == 46 || s == 63)
@@ -42,17 +39,13 @@ int main(void)
             sentencecount += 1;
         }
     }
-    //printf("%f sentence(s)\n", sentencecount);
 
     // tabulating L and S values per 100 words
     float L = lettercount * (100 / wordcount);
-    //printf("L is %f\n", L);
     float S = sentencecount * (100 / wordcount);
-    //printf("S is %f\n", S);
 
     // Coleman-Liau index
     int index = (0.0588 * L) - (0.296 * S) - 15.8;
-    printf("Coleman-Liau index: %i\n", index);
 
     if (index >= 16)
     {
--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -6,7 +6,16 @@ def main():
         print("Usage: python dna.py data.csv sequence.txt")
 
     else:
-        file open(argv[1], "r")
+        with open(argv[1], newline='') as csvfile:
+            db = csv.reader(csvfile, delimiter = ' ', quotechar = "|")
+        print(db)
+
+
+        s = open(argv[2], "r")
+        seq = s.read()
+
+
+
 
 
 if __name__ == "__main__":
